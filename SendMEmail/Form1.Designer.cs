950d74ad89c92a5b0615cda2db1ac99772f717bc
Lochie Denholm <lochlan.denholm@informatel.com>
2019-09-11 14:59:53 +1000

    foundational smtp desktop client

--- SendMEmail/Form1.Designer.cs
+++ SendMEmail/Form1.Designer.cs
@@ -28,13 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.lblSubject = new System.Windows.Forms.Label();
+            this.lblBody = new System.Windows.Forms.Label();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.txtBody = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTo
+            // 
+            this.lblTo.AutoSize = true;
+            this.lblTo.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTo.Location = new System.Drawing.Point(14, 32);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(37, 18);
+            this.lblTo.TabIndex = 0;
+            this.lblTo.Text = "To:";
+            // 
+            // lblSubject
+            // 
+            this.lblSubject.AutoSize = true;
+            this.lblSubject.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubject.Location = new System.Drawing.Point(14, 87);
+            this.lblSubject.Name = "lblSubject";
+            this.lblSubject.Size = new System.Drawing.Size(80, 18);
+            this.lblSubject.TabIndex = 1;
+            this.lblSubject.Text = "Subject:";
+            // 
+            // lblBody
+            // 
+            this.lblBody.AutoSize = true;
+            this.lblBody.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBody.Location = new System.Drawing.Point(5, 228);
+            this.lblBody.Name = "lblBody";
+            this.lblBody.Size = new System.Drawing.Size(58, 18);
+            this.lblBody.TabIndex = 2;
+            this.lblBody.Text = "Body:";
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(100, 33);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(192, 20);
+            this.txtTo.TabIndex = 3;
+            this.txtTo.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(100, 88);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(192, 20);
+            this.txtSubject.TabIndex = 4;
+            // 
+            // txtBody
+            // 
+            this.txtBody.Location = new System.Drawing.Point(69, 229);
+            this.txtBody.Multiline = true;
+            this.txtBody.Name = "txtBody";
+            this.txtBody.Size = new System.Drawing.Size(223, 142);
+            this.txtBody.TabIndex = 5;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Location = new System.Drawing.Point(130, 383);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 34);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(327, 429);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtBody);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.lblBody);
+            this.Controls.Add(this.lblSubject);
+            this.Controls.Add(this.lblTo);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTo;
+        private System.Windows.Forms.Label lblSubject;
+        private System.Windows.Forms.Label lblBody;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.TextBox txtBody;
+        private System.Windows.Forms.Button btnSend;
     }
 }
 
--- SendMEmail/Form1.cs
+++ SendMEmail/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
 
 namespace SendMEmail
 {
@@ -15,6 +17,44 @@ namespace SendMEmail
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Pulling data from text boxes into MailMessage obj.
+                MailMessage msg = new MailMessage();
+                msg.From = new MailAddress("loki@informatel.com");
+                msg.To.Add(txtTo.Text);
+                msg.Subject = txtSubject.Text;
+                msg.Body = txtBody.Text;
+
+                // Creating smtp client
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                System.Net.NetworkCredential ntcd = new NetworkCredential();
+                ntcd.UserName = "loki@informatel.com";
+                ntcd.Password = "*********";
+                smtp.Credentials = ntcd;
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.EnableSsl = true;
+                smtp.Port = 587;
+                smtp.Send(msg);
+
+
+                MessageBox.Show("Your email has been sent.");
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
